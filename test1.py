66926d7ae2b93a4af1a5c519b663e13dca2467bd
Mohit-Chowdhary <mohit24c@gmail.com>
2025-02-15 21:07:05 +0530

    test 1

--- test1.py
+++ test1.py
@@ -1,13 +1,12 @@
 from picozero import pico_led,LED, Button
 from time import sleep
 
-button = Button(15)
+
 
 while True:
-    if button.is_released:
-        print("Button Released")
-        pico_led.off()
-    else:
-        print("Button Pressed")
-        pico_led.on()
-    sleep(0.1)
+    print("Button Released")
+    pico_led.off()
+    sleep(2)
+    print("Button Pressed")
+    pico_led.on()
+    sleep(2)
